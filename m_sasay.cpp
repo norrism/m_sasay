e701ecff33f9a07a3ac7d8e5c8d734e1834cdabd
Michael Norris <michael.norris93@gmail.com>
2015-12-01 22:45:41 -0500

    Update m_sasay.cpp

--- m_sasay.cpp
+++ m_sasay.cpp
@@ -6,6 +6,11 @@
  * This file contains a third party module for InspIRCd.  You can
  * redistribute it and/or modify it under the terms of the GNU General Public
  * License as published by the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
  * 
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
